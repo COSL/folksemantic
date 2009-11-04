576a52fceeeb3a08508a13703852e85a1877c52a
Joel Duffin <Joel@joel-duffins-macbook.local>
2009-11-04 15:55:45 -0700

    Modified pids paths

--- config/initializers/muck_raker.rb
+++ config/initializers/muck_raker.rb
@@ -1,7 +1,8 @@
 ENV['RAILS_ENV'] = ENV['RAILS_ENV'] || GlobalConfig.rails_env  
 SOLR_CONFIG_PATH = "#{RAILS_ROOT}/config/solr"
 SOLR_LOGS_PATH = "#{RAILS_ROOT}/log"
-SOLR_PIDS_PATH = "#{RAILS_ROOT}/tmp/pids"
+SOLR_PIDS_PATH = "#{RAILS_ROOT}/tmp/pids/solr"
+RAKER_PIDS_PATH = "#{RAILS_ROOT}/tmp/pids/raker"
 if ENV['RAILS_ENV'] == "production"
   SOLR_DATA_PATH = "#{RAILS_ROOT}/../../shared/solr_indexes"
 else
