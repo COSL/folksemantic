13b998d6f8822db0282613590147854a36525810
Justin Ball <justinball@gmail.com>
2009-12-10 17:54:03 -0700

    updated versions

--- config/environment.rb
+++ config/environment.rb
@@ -53,7 +53,7 @@ Rails::Initializer.run do |config|
   config.gem 'muck-contents', :lib => 'muck_contents', :version => '>=0.2.11'
   config.gem 'muck-blogs', :lib => 'muck_blogs', :version => '>=0.1.8'
   config.gem 'muck-invites', :lib => 'muck_invites', :version => '>=0.1.5'
-  config.gem 'muck-oauth', :lib => 'muck_oauth', :version => '>=0.1.0'
+  config.gem 'muck-oauth', :lib => 'muck_oauth', :version => '>=0.1.1'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
