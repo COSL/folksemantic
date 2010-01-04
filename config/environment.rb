00ea042bc4afd47c7df11fa09357fbbce5aa26ae
Justin Ball <justinball@gmail.com>
2010-01-04 09:32:27 -0700

    updated versions

--- config/environment.rb
+++ config/environment.rb
@@ -43,7 +43,7 @@ Rails::Initializer.run do |config|
   config.gem "muck-solr", :lib => 'acts_as_solr', :version => '>=0.4.5'
   config.gem "muck-feedbag", :lib => 'feedbag', :version => '>=0.6.0'
   config.gem "muck-raker", :lib => 'muck_raker', :version => '>=0.3.7'
-  config.gem "muck-services", :lib => 'muck_services', :version => '>=0.1.25'
+  config.gem "muck-services", :lib => 'muck_services', :version => '>=0.1.26'
   config.gem 'muck-users', :lib => 'muck_users', :version => '>=0.2.19'
   config.gem 'muck-activities', :lib => 'muck_activities', :version => '>=0.1.25'
   config.gem 'muck-comments', :lib => 'muck_comments', :version => '>=0.1.17'
