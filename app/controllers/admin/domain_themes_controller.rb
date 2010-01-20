7815684dbb170ca81dbd160a0fa09216ec4b6df4
Justin Ball <justinball@gmail.com>
2010-01-19 18:59:08 -0700

    removed disguise

--- app/controllers/admin/domain_themes_controller.rb
+++ app/controllers/admin/domain_themes_controller.rb
@@ -1,4 +1,4 @@
-class Admin::DomainThemesController < Admin::Disguise::DomainThemesController
+class Admin::DomainThemesController < ActionController::Base #Admin::Disguise::DomainThemesController
   before_filter :login_required
   before_filter :admin_access_required
 end
