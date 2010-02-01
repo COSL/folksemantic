e899b3712a2b259bce22cb998f1c21e407ca5f13
Justin Ball <justinball@gmail.com>
2010-02-01 13:58:01 -0700

    updated docs.  Synced in latest files

--- config/initializers/admin.rb
+++ config/initializers/admin.rb
@@ -1,5 +1,6 @@
 # Add admin links
-MuckEngine.add_muck_admin_nav_item(I18n.translate('muck.admin.themes'), '/admin/theme/edit', '/images/admin/theme.gif')
+# MuckEngine.add_muck_admin_nav_item(I18n.translate('muck.admin.themes'), '/admin/theme/edit', '/images/admin/theme.gif')
+MuckEngine.add_muck_admin_nav_item(I18n.translate('muck.admin.themes'), '/admin/theme/edit')
 
 # Add items to the dashboard
 #MuckEngine.add_muck_dashboard_item('admin/users/dashboard_widget') rescue nil
