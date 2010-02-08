50b6d2eb6586b0e556c23b5a5474fe5075c7e3f3
Justin Ball <justinball@gmail.com>
2010-02-08 11:25:16 -0700

    backed off version on users

--- config/environment.rb
+++ config/environment.rb
@@ -47,7 +47,7 @@ Rails::Initializer.run do |config|
   config.gem "muck-feedbag", :lib => 'feedbag', :version => '>=0.6.0'
   config.gem "muck-raker", :lib => 'muck_raker', :version => '>=0.3.7'
   config.gem "muck-services", :lib => 'muck_services', :version => '>=0.1.35'
-  config.gem 'muck-users', :lib => 'muck_users', :version => '>=0.3.4'
+  config.gem 'muck-users', :lib => 'muck_users', :version => '>=0.3.3'
   config.gem 'muck-activities', :lib => 'muck_activities', :version => '>=0.1.26'
   config.gem 'muck-comments', :lib => 'muck_comments', :version => '>=0.1.18'
   config.gem 'muck-profiles', :lib => 'muck_profiles', :version => '>=0.1.18'
