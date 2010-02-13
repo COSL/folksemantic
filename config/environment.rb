819cb90f0e09b5b75e5a83b6f604345da4937550
Justin Ball <justinball@gmail.com>
2010-02-13 15:24:26 -0700

    updated gems

--- config/environment.rb
+++ config/environment.rb
@@ -38,25 +38,25 @@ Rails::Initializer.run do |config|
   config.gem 'httparty', :version => '~>0.4.5'
   config.gem "oauth"
   config.gem "oauth-plugin"
-  config.gem 'overlord', :version => '>=0.1.19'
+  config.gem 'overlord', :version => '>=0.1.20'
   config.gem 'babelphish', :version => '>=0.2.7'
   config.gem 'uploader', :version => '>=0.2.7'
   config.gem 'disguise', :version => '>=0.4.0'
-  config.gem 'muck-engine', :lib => 'muck_engine', :version => '>=0.4.4'
-  config.gem "muck-solr", :lib => 'acts_as_solr', :version => '>=0.4.5'
+  config.gem 'muck-engine', :lib => 'muck_engine', :version => '>=0.4.8'
+  config.gem "muck-solr", :lib => 'acts_as_solr', :version => '>=0.4.7'
   config.gem "muck-feedbag", :lib => 'feedbag', :version => '>=0.6.0'
-  config.gem "muck-raker", :lib => 'muck_raker', :version => '>=0.3.7'
+  config.gem "muck-raker", :lib => 'muck_raker', :version => '>=0.3.13'
   config.gem "muck-services", :lib => 'muck_services', :version => '>=0.1.38'
-  config.gem 'muck-users', :lib => 'muck_users', :version => '>=0.3.4'
-  config.gem 'muck-activities', :lib => 'muck_activities', :version => '>=0.1.26'
+  config.gem 'muck-users', :lib => 'muck_users', :version => '>=0.3.7'
+  config.gem 'muck-activities', :lib => 'muck_activities', :version => '>=0.1.27'
   config.gem 'muck-comments', :lib => 'muck_comments', :version => '>=0.1.18'
-  config.gem 'muck-profiles', :lib => 'muck_profiles', :version => '>=0.1.18'
+  config.gem 'muck-profiles', :lib => 'muck_profiles', :version => '>=0.2.0'
   config.gem 'muck-friends', :lib => 'muck_friends', :version => '>=0.1.17'
   config.gem 'muck-shares', :lib => 'muck_shares', :version => '>=0.1.8'
   config.gem 'muck-contents', :lib => 'muck_contents', :version => '>=0.2.17'
   config.gem 'muck-blogs', :lib => 'muck_blogs', :version => '>=0.1.8'
   config.gem 'muck-invites', :lib => 'muck_invites', :version => '>=0.1.5'
-  config.gem 'muck-oauth', :lib => 'muck_oauth', :version => '>=0.1.4'
+  config.gem 'muck-oauth', :lib => 'muck_oauth', :version => '>=0.1.5'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
