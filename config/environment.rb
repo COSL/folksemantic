d7bf91fb81e7f16dbd21b5e4e3bff9652f5c9759
Joel Duffin <Joel@Joel-Duffins-iMac.local>
2010-02-26 13:16:27 -0700

    updated gem dependencies

--- config/environment.rb
+++ config/environment.rb
@@ -45,7 +45,7 @@ Rails::Initializer.run do |config|
   config.gem "muck-solr", :lib => 'acts_as_solr', :version => '>=0.4.7'
   config.gem "muck-feedbag", :lib => 'feedbag', :version => '>=0.6.0'
   config.gem "muck-raker", :lib => 'muck_raker', :version => '>=0.3.13'
-  config.gem "muck-services", :lib => 'muck_services', :version => '>=0.1.39'
+  config.gem "muck-services", :lib => 'muck_services', :version => '>=0.1.40'
   config.gem 'muck-users', :lib => 'muck_users', :version => '>=0.3.9'
   config.gem 'muck-activities', :lib => 'muck_activities', :version => '>=0.1.27'
   config.gem 'muck-comments', :lib => 'muck_comments', :version => '>=0.1.18'
