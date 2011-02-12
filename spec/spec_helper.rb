9cff18c3b585e9861445fec98a162903da0fb577
Justin Ball <justinball@gmail.com>
2011-02-12 12:09:54 -0700

    changed to rspec. fixed gemfile

---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+$:.reject! { |e| e.include? 'TextMate' }
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+
+require 'muck_test_helper'
+
+require File.expand_path("../factories", __FILE__)
